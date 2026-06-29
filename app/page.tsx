58e72ddb3ad94fcad43bfe5b0ba986e038c17b94
iskoczykowski <i.skoczykowski@gmail.com>
2026-06-29 09:36:46 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1209,7 +1209,13 @@ cursor:'pointer'
 </div>
 ))}
             <div key={Math.random()} style={{display:'grid',gridTemplateColumns:'1fr 1fr 100px',gap:6,marginBottom:4}}>
-              <select style={search}><option>{x}</option></select>
+              <select style={search}>
+  <option>Horren</option>
+  <option>Raamdecoratie</option>
+  <option>Rolluiken</option>
+  <option>Screens</option>
+  <option>Zonwering</option>
+</select>
               <select style={search}>
                 <option>Aluminium inzet hor</option>
                 <option>Plissé</option>
