e7e710b210ae6f4ffdaa5391aabd665500c39cbe
iskoczykowski <i.skoczykowski@gmail.com>
2026-07-03 22:59:26 +0200

    Add CustomersPage component with header and module

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,1 +1,16 @@
+'use client';
 
+import CustomersModule from '../components/CustomersModule';
+import AppHeader from '../components/AppHeader';
+
+export default function CustomersPage() {
+  return (
+    <main className="min-h-screen bg-slate-100">
+      <div className="mx-auto max-w-7xl p-6">
+        <AppHeader />
+
+        <CustomersModule />
+      </div>
+    </main>
+  );
+}
