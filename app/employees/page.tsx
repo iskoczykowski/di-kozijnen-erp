bddd124ed4f774f504cdf87294666ee10378876d
iskoczykowski <i.skoczykowski@gmail.com>
2026-07-03 23:33:19 +0200

    Update page.tsx

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -1,1 +1,15 @@
+'use client';
 
+import AppHeader from '../components/AppHeader';
+import EmployeesModule from '../components/EmployeesModule';
+
+export default function EmployeesPage() {
+  return (
+    <main className="min-h-screen bg-slate-100">
+      <div className="mx-auto max-w-7xl p-6">
+        <AppHeader />
+        <EmployeesModule />
+      </div>
+    </main>
+  );
+}
