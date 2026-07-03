30d636bcba3c46321b4add4b3394f80e304ab996
iskoczykowski <i.skoczykowski@gmail.com>
2026-07-03 23:19:52 +0200

    Update page.tsx

--- app/production/page.tsx
+++ app/production/page.tsx
@@ -1,1 +1,15 @@
+'use client';
 
+import AppHeader from '../components/AppHeader';
+import ProductionModule from '../components/ProductionModule';
+
+export default function ProductionPage() {
+  return (
+    <main className="min-h-screen bg-slate-100">
+      <div className="mx-auto max-w-7xl p-6">
+        <AppHeader />
+        <ProductionModule />
+      </div>
+    </main>
+  );
+}
